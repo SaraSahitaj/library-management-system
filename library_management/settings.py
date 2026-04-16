e3c073e05d53618ef2c1dbdc003ae688971a7767
Sara Sahitaj <sahitajsara@gmail.com>
2026-04-17 01:44:40 +0200

    remove admin code from setting

--- library_management/settings.py
+++ library_management/settings.py
@@ -97,13 +97,3 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(
-        username='admin',
-        email='admin@admin.com',
-        password='admin123@'
-    )
